984e330316ab18dcba984d7684b31b035af42e44
Denish <kukadiyadenish@gmail.com>
2020-07-24 13:36:30 +0530

    After_analtyic

--- app.js
+++ app.js
@@ -1,44 +1,74 @@
-const apiKey  = 'd486450732f74975b10415a4704fda06';
+const apiKey = 'd486450732f74975b10415a4704fda06';
 const main = document.querySelector('main');
 const sourceSelector = document.querySelector('#sourceSelector');
-const defaultSource = "the-times-of-india";
+let defaultSource= "the-times-of-india";
 
 
-window.addEventListener('load', async e=>{
-    updateNews();
-    sourceSelector.value = defaultSource;
-    await updateSources();
-    
-    sourceSelector.addEventListener('change', e =>{
-        updateNews(e.target.value);
+window.addEventListener('load', async e => {
+  navigator.serviceWorker.register('sw.js')
+      .then(reg => {
+        console.log('Service Worker Registered!', reg);
+      })
+      .catch(err => {
+        console.log('Service Worker registration failed: ', err);
+      });
+
+    // Request notification permission
+    Notification.requestPermission(function(status) {
+      console.log('Notification permission status:', status);
     });
+ 
+  updateNews();
+  // const params = new URLSearchParams(document.location.search);
+  // const s = params.get("s")
+  await updateSources();
+  // setSelectedIndex(document.getElementById("sourceSelector"), s);
+ //
+ //  if (s == null) {
+ //    defaultSource = "the-times-of-india";
+ //    await updateSources();
+ //   updateNews(defaultSource);
+ //
+ // }
+ //  else {
+ //    console.log('me');
+ //    defaultSource = s;
+ //  }
+  sourceSelector.value = defaultSource;
 
-    if('serviceWorker' in navigator){
-        try{
-            navigator.serviceWorker.register('sw.js');
 
-        }
-        catch(error){
-console.log("sw failed");
-        }
-
+  sourceSelector.addEventListener('change', e => {
+    ga('send', 'event', 'Good News', 'click', `${e.target.value}`);
+    updateNews(e.target.value);
+    // location.href = `detail.html?s=${e.target.value}`;
+    // setSelectedIndex(document.getElementById("sourceSelector"), s);
+  });
+  if ('serviceWorker' in navigator) {
+    try {
+      navigator.serviceWorker.register('sw.js');
+      console.log('sw register');
     }
+    catch (error) {
+      console.log("sw failed");
+    }
+
+  }
 });
-async function updateSources(){
-    const res = await fetch('https://newsapi.org/v2/sources?country=us&apiKey=d486450732f74975b10415a4704fda06');
-    const json = await res.json();
+async function updateSources() {
+  const res = await fetch('https://newsapi.org/v2/sources?country=us&apiKey=d486450732f74975b10415a4704fda06');
+  const json = await res.json();
 
-    sourceSelector.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
+  sourceSelector.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
 
 }
-async function updateNews(source = defaultSource){
+async function updateNews(source = defaultSource) {
 
-    const res = await fetch(`http://newsapi.org/v2/top-headlines?sources=${source}&apiKey=d486450732f74975b10415a4704fda06`);
-    const json = await res.json();
-    main.innerHTML = json.articles.map(createArticle).join('\n');
+  const res = await fetch(`http://newsapi.org/v2/top-headlines?sources=${source}&apiKey=d486450732f74975b10415a4704fda06`);
+  const json = await res.json();
+  main.innerHTML = json.articles.map(createArticle).join('\n');
 }
-function createArticle(article){
-    return `
+function createArticle(article) {
+  return `
     <style>
     h2 {
      font-family: Georgia, 'Times New Roman', Times, serif;
@@ -48,7 +78,7 @@ function createArticle(article){
      text-decoration: none;
      color: inherit;
    }
-   
+
     img {
      width: 100%;
    }
@@ -56,10 +86,53 @@ function createArticle(article){
         <div class = "article">
         <a href="${article.url}">
         <h2>${article.title}</h2>
-        <img src="${article.urlToImage ? article.urlToImage : ''}">
         <p>${article.description}</p>
         </a>
-        
+
         </div>
 `;
+}
+
+
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+
+
+function setSelectedIndex(s, valsearch) {
+
+  // Loop through all the items in drop down list
+
+  for (let i = 0; i < s.options.length; i++) {
+    if (s.options[i].value == valsearch) {
+      s.options[i].selected = true;
+      break;
+    }
+  }
+  return;
 }
--- sw.js
+++ sw.js
@@ -1,42 +1,50 @@
-const staticAssets = [
-'./',
-'./styles.css',
-'./app.js',
-'./fallback.json',
-'./images/fetch-dog.jpg'
+importScripts('./analytics-helper.js');
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+workbox.googleAnalytics.initialize({
+  // Set custom dimension in slot index 1 to a value of 'offline'
+  parameterOverrides: {
+    cd1: 'offline'
+  },
+  // Set custom metric in slot index 1 to qt/1000 (i.e. queued seconds)
+  hitFilter: params => {
+   const queueTimeInSeconds = Math.round(params.get('qt') / 1000)
+   params.set('cm1', queueTimeInSeconds)
+  }
+});
+const staticAssets = [
+  './',
+  './styles.css',
+  './app.js',
+  './fallback.json'
 ];
 
 
+
 self.addEventListener('install', async event =>{
-    const cache = await caches.open('news-static');
-    cache.addAll(staticAssets);
-
+  const cache = await caches.open('news-static');
+  await cache.addAll(staticAssets);
 });
 
-self.addEventListener('fetch', event =>{
-    const req = event.request;
+self.addEventListener('fetch', async e => {
+  const req = e.request;
     const url = new URL(req.url);
-
-    if(url.origin == location.origin){
-        
-        event.respondWith(cacheFirst(req));
+    if (url.origin === location.origin) {
+      e.respondWith(cacheFirst(req));
+    } else {
+      console.log('hello');
+      e.respondWith(networkFirst(req));
     }
-    else{
-       
-        event.respondWith(networkFirst(req));
-    } 
-
 });
 
-async function cacheFirst(req){
 
-    const cachedResponse = await caches.match(req);
-     
-    return cachedResponse || fetch(req);
+async function cacheFirst(req) {
+  const cache = await caches.open('news-dynamic');
+  const cached = await cache.match(req);
+  return cached || fetch(req);
 }
  async function networkFirst(req){
-
     const cache = await caches.open('news-dynamic');
     try {
         const res = await fetch(req);
@@ -47,3 +55,55 @@ async function cacheFirst(req){
         return cachedResponse || await caches.match('./fallback.json');
     }
 }
+
+//
+// async function networkAndCache(req) {
+//   const cache = await caches.open('news-dynamic');
+//   try {
+//     const fresh = await fetch(req);
+//     await cache.put(req, fresh.clone());
+//     return fresh;
+//   } catch (e) {
+//     const cached = await cache.match(req);
+//     return cached;
+//   }
+// }
+
+
+
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  console.log('Closed notification: ' + primaryKey);
+  // TODO Send notification close event
+});
+
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  notification.close();
+  e.waitUntil(
+    Promise.all([
+      clients.openWindow('pages/page' + primaryKey + '.html'),
+      // TODO Send notification click event
+      sendAnalyticsEvent('click', 'notification')
+    ])
+  );
+});
+
+self.addEventListener('push', function(e) {
+  var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/notification-flat.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '-push-notification'
+    }
+  };
+  e.waitUntil(Promise.all([
+      self.registration.showNotification('Hello world!', options),
+      // TODO Send push recieved event
+    ])
+  );
+});
